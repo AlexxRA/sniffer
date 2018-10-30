ba894fa8066f16b15d09ce1a8d5014bb5c3d24ed
GeraVM98 <geraatlas98@gmail.com>
2018-10-29 19:06:41 -0600

    form inicio

---
+++ Sniffer_0.1/Inicio.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Sniffer_0._1
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBuffer = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboIp = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBuffer);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboIp);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(10, 13);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(376, 203);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select startup options:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 156);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(253, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Old packet will be discarted.";
+            // 
+            // comboBuffer
+            // 
+            this.comboBuffer.FormattingEnabled = true;
+            this.comboBuffer.Location = new System.Drawing.Point(15, 122);
+            this.comboBuffer.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBuffer.Name = "comboBuffer";
+            this.comboBuffer.Size = new System.Drawing.Size(209, 29);
+            this.comboBuffer.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 101);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(335, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select packets number to be buffered:";
+            // 
+            // comboIp
+            // 
+            this.comboIp.FormattingEnabled = true;
+            this.comboIp.Location = new System.Drawing.Point(15, 57);
+            this.comboIp.Margin = new System.Windows.Forms.Padding(4);
+            this.comboIp.Name = "comboIp";
+            this.comboIp.Size = new System.Drawing.Size(209, 29);
+            this.comboIp.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select IP Address:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(94, 279);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(189, 48);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(399, 361);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Inicio";
+            this.Text = "Inicio";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBuffer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
---
+++ Sniffer_0.1/Inicio.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace Sniffer_0._1
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            InitBufferSizeChooserCtrl();
+            InitIPChooserCtrl();
+        }
+
+        private void InitIPChooserCtrl()
+        {
+            string hostName = Dns.GetHostName();
+            IPAddress[] IPs = Dns.GetHostAddresses(hostName);
+
+            foreach (IPAddress ip in IPs)
+            {
+                _view.AddIPItem(ip.ToString());
+            }
+        }
+
+        private void InitBufferSizeChooserCtrl()
+        {
+            for (int i = 100; i < 1000; i += 100)
+                _view.AddBufferSizeItem(i.ToString());
+
+            for (int j = 1000; j < 100000; j += 1000)
+                _view.AddBufferSizeItem(j.ToString());
+
+            _view.SelectedBufferSize = 1000;
+        }
+    }
+}
